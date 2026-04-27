d388d80e18fb96001cd31dd22d009d3f31fd7360
Techsation <techsation101@gmail.com>
2026-04-27 16:08:39 -0600

    allowed variables to be accessable within other variables via the math function, removed useless saving variable as number, added test script

--- Main.rb
+++ Main.rb
@@ -3,6 +3,7 @@ puts "Please enter your Aquamarine code here:"
 puts ""
 print ""
 code = gets.chomp
+#code = File.read(filename)
 
 def slice_from_nearest_ampersand(array, target_index)
   # Find the last '&' before the target. If nil, use 0.
@@ -19,9 +20,14 @@ end
 temp0 = ""
 temp1 = ""
 temp2 = ""
+temp3 = ""
+temp4 = ""
+temp5 = ""
 tempArr = []
 tempNum0 = 0.0
 tempNum1 = 0.0
+
+perm0 = ""
 
 localInCode = 0
 
@@ -32,6 +38,21 @@ variables = Hash.new("Error Missing Variable")
 
 output = ""
 outputNum =0.0
+
+def variable_referencer(index_ahead, list, local, temp, variablesList, otherTemp = nil)
+  for variableName in list[local+index_ahead..-1]
+    if variableName == "\\"
+      break
+    end
+    temp += variableName
+    #puts temp
+  end
+  if otherTemp != nil
+    otherTemp << temp
+  end
+  variablesList[temp]
+end
+
 #puts codeAsList
 for char in codeAsList do
   #print
@@ -45,13 +66,7 @@ for char in codeAsList do
               break
             end
             if printables == "\\"
-              for variableName in codeAsList[localInCode+3..-1]
-                if variableName == "`"
-                  break
-                end
-                temp0 += variableName
-              end
-              output += variables[temp0].to_s
+              output += variable_referencer(3, codeAsList, localInCode, temp0, variables).to_s
               break
             end
             output += printables
@@ -66,13 +81,7 @@ for char in codeAsList do
               break
             end
             if printables == "\\"
-              for variableName in codeAsList[localInCode+3..-1]
-                if variableName == "`"
-                  break
-                end
-                temp0 += variableName
-              end
-              output += variables[temp0]
+              output += variable_referencer(3, codeAsList, localInCode, temp0, variables).to_s
               break
             end
             output += printables
@@ -104,31 +113,28 @@ for char in codeAsList do
       end
       variables[temp0] = temp1
     else
-      if codeAsList[localInCode + 2] == "n"
-        for value in codeAsList[localInCode+3..-1]
-        if value == "&"
-          break
-        end
-        temp1 += value
-        end
-        tempArr = slice_from_nearest_ampersand(codeAsList, localInCode)
-        tempArr.delete("&")
-        tempArr.pop
-        for value in tempArr
-          temp0 += value
-        end
-        variables[temp0] = temp1.to_f
-      end
       if codeAsList[localInCode + 2] == "m"
         for number in codeAsList[localInCode+3..-1]
           if number == "+" || number == "-" || number == "*" || number == "/" || number == "^" 
             break
           end
-          temp0 += number
+          if number == "\\"
+            perm0 += variable_referencer(4, codeAsList, localInCode, temp3, variables, temp5)
+            #puts temp0
+            #puts temp5
+            break
+          end
+          perm0 += number
         end
-        tempNum0 = temp0.to_f
-        for number in codeAsList[localInCode+4+temp0.length..-1]
+        tempNum0 = perm0.to_f
+        for number in codeAsList[localInCode+4+perm0.length..-1]
           if number == "&"
+            break
+          end
+          if number == "\\"
+            temp1 = ""
+            temp1 += variable_referencer(7+temp5.length, codeAsList, localInCode, temp4, variables)
+            #puts temp1
             break
           end
           temp1 += number
@@ -139,24 +145,42 @@ for char in codeAsList do
         for value in tempArr
           temp2 += value
         end
-        if codeAsList[localInCode+3+temp0.length] == "+"
-          variables[temp2] = temp0.to_f + temp1.to_f
-        elsif codeAsList[localInCode+3+temp0.length] == "-"
-          variables[temp2] = temp0.to_f - temp1.to_f
-        elsif codeAsList[localInCode+3+temp0.length] == "*"
-          variables[temp2] = temp0.to_f * temp1.to_f
-        elsif codeAsList[localInCode+3+temp0.length] == "/"
-          variables[temp2] = temp0.to_f / temp1.to_f
-        elsif codeAsList[localInCode+3+temp0.length] == "^"
-          variables[temp2] = temp0.to_f ** temp1.to_f
+        #puts temp2
+        #puts variable_referencer(4+temp0.length, codeAsList, localInCode, temp3, variables)
+        if codeAsList[localInCode+5+temp5.length] == "+"
+          variables[temp2] = perm0.to_f + temp1.to_f
+        elsif codeAsList[localInCode+5+temp5.length] == "-"
+          variables[temp2] = perm0.to_f - temp1.to_f
+        elsif codeAsList[localInCode+5+temp5.length] == "*"
+          variables[temp2] = perm0.to_f * temp1.to_f
+        elsif codeAsList[localInCode+5+temp5.length] == "/"
+          variables[temp2] = perm0.to_f / temp1.to_f
+        elsif codeAsList[localInCode+5+temp5.length] == "^"
+          variables[temp2] = perm0.to_f ** temp1.to_f
+        end
+        perm0 = ""
+        #puts perm0.to_f + temp1.to_f
+        if codeAsList[localInCode+3+perm0.length] == "+"
+          variables[temp2] = perm0.to_f + temp1.to_f
+        elsif codeAsList[localInCode+3+perm0.length] == "-"
+          variables[temp2] = perm0.to_f - temp1.to_f
+        elsif codeAsList[localInCode+3+perm0.length] == "*"
+          variables[temp2] = perm0.to_f * temp1.to_f
+        elsif codeAsList[localInCode+3+perm0.length] == "/"
+          variables[temp2] = perm0.to_f / temp1.to_f
+        elsif codeAsList[localInCode+3+perm0.length] == "^"
+          variables[temp2] = perm0.to_f ** temp1.to_f
         end
       end
     end
-    #puts variables
+    puts variables
   end
   temp0 = ""
   temp1 = ""
   temp2 = ""
+  temp3 = ""
+  temp4 = ""
+  temp5 = ""
 
   localInCode += 1
 end
--- README.md
+++ README.md
@@ -7,8 +7,6 @@ p\`_\`(print change _ to what you would like to print)
 
 _=#(set_value change _ to any variable and # to any value (defaults to string))
 
-\`n#(changes_value_to_number change # to any number)
+\\#\\(references_variable change # to any variable)
 
-\\#(references_variable change # to any variable)
-
-`m#+#(math change # to any number and + to +, -, *, /, or ^)
+`m#+#(math change # to any number and + to +, -, *, /, or ^ (if used with variable both 'numbers' have to be variables))
